88ad507f1634f247ea6b7e74ac86430bf84d97eb
szrrizvi <szrrizvi@ict625-a.cs.ucalgary.ca>
2018-01-09 14:14:44 -0700

    first commit

---
+++ src/main/java/ca/ucalgary/ispia/arebac/App.java
@@ -0,0 +1,13 @@
+package ca.ucalgary.ispia.arebac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
